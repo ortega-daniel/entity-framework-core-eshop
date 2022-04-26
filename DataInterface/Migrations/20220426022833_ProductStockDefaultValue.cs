b0d69b32bdbd069ba3b31057fafa1973d3fbd679
ortega-daniel <ic.danielortega@gmail.com>
2022-04-25 20:47:51 -0700

    Migrations

---
+++ DataInterface/Migrations/20220426022833_ProductStockDefaultValue.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataInterface.Migrations
+{
+    public partial class ProductStockDefaultValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Stock",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Stock",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 0);
+        }
+    }
+}
