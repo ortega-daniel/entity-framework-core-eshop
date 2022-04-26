b89a385a48fe64c08d07f54b87b95eed18d226a0
ortega-daniel <ic.danielortega@gmail.com>
2022-04-25 20:46:44 -0700

    Dtos

---
+++ BusinessLayer/Dtos/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public string Sku { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public int SubdepartmentId { get; set; }
+    }
+}
---
+++ BusinessLayer/Dtos/SubdepartmentDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Dtos
+{
+    public class SubdepartmentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
