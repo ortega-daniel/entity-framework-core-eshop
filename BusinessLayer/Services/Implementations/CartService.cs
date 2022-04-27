6906034861ce4a9c06570a495c0947e598282bc6
ortega-daniel <ic.danielortega@gmail.com>
2022-04-27 09:26:46 -0700

    Cart service

---
+++ BusinessLayer/Services/Implementations/CartService.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services.Implementations
+{
+    public class CartService : ICartService
+    {
+        public void AddProduct() 
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
